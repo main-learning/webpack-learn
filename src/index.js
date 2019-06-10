bc62a32be830851e28ec1f5c80027f203eb2b2fa
Laba Subagia <labasubagia22@gmail.com>
2019-06-10 12:55:00 +0800

    asset-management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 
+import Icon from './icon.svg';
+import Data from './data.xml';
+import './style.css';
+
 function component() {
 
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Laba'], ' ');
+    element.classList.add('hello');
     
+    const myIcon = new Image();
+    myIcon.src = Icon; 
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,47 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // css loader
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      // image loader
+      {
+        test: /\.(jpg|png|svg|gif|webp)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      // font loader
+      {
+        test: /\.(ttf|woff|woff2|otf|eot)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      // csv loader
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader',
+        ],
+      },
+      // xml loader
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader',
+        ],
+      },
+    ],
   },
 };
