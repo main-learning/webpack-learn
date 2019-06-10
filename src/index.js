6fde3bda7192dc1ce6835b18f10e01195d85cb88
Laba Subagia <labasubagia22@gmail.com>
2019-06-10 13:41:33 +0800

    development

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'Laba'], ' ');
+    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
     btn.innerHTML = 'Click me and check the console!';
 
     btn.onclick = printMe;
